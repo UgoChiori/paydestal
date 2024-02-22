8a5a84a3ee5d4a55a03c1a8b76f22bd9ceaace1c
checkr <ugochiori@gmail.com>
2024-02-22 22:39:29 +0100

    hamburger

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,16 +1,23 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import { Link } from "react-router-dom";
 import "./navigation.css"
 import { navLinks } from "../constants/navLinks";
 
 const NavigationBar: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
+
   return (
     <nav>
       <div className="nav-left">
         <div className="logo">
           <img src="/images/logo.svg" alt="Paydestal Logo" />
         </div>
-        <div className="nav-links-container">
+        <div className={`nav-links-container ${isMenuOpen ? "open" : ""}`}>
           {navLinks.map((link: { name: string, route: string }, index: number): ReactNode => {
             return (
               <Link key={index} to={link.route}>{link.name}</Link>
@@ -26,6 +33,11 @@ const NavigationBar: React.FC = () => {
         <button className="reg-button">
           <Link to="/register">Sign Up</Link>
         </button>
+      </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className={`line ${isMenuOpen ? "open" : ""}`}></div>
+        <div className={`line ${isMenuOpen ? "open" : ""}`}></div>
+        <div className={`line ${isMenuOpen ? "open" : ""}`}></div>
       </div>
     </nav>
   );
